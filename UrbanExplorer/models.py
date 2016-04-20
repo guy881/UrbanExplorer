959a2f3f5e4843f684e562264cd3998b4c8bf47d
Paweł <muchap1@ee.pw.edu.pl>
2016-04-20 21:01:17 +0200

    First models for venue and riddle created, basic configurations set

--- UrbanExplorer/models.py
+++ UrbanExplorer/models.py
@@ -2,4 +2,20 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Venue(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
+class Riddle(models.Model):
+    name = models.CharField(max_length=100)
+    venue = models.ForeignKey(Venue, on_delete=models.CASCADE)  # every riddle is assigned to particular venue
+    category = models.CharField(max_length=40)
+    rating = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+    # After changing model run: python manage.py makemigrations UrbanExplorer
