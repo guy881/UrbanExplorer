b662786b38c0ed3762a08a83dcc08d764bc40193
Konrad <konrad5446@wp.pl>
2016-10-15 17:41:17 +0200

    login logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,8 +5,10 @@ from django.contrib import admin
 from .views import *
 
 urlpatterns = [
-    url(r'^api/login/$', UserLoginAPIView.as_view(), name='login'),
-    url(r'^api/register/$', UserCreateAPIView.as_view(), name='register'),
+    url(r'^api/login/$', UserLoginAPIView.as_view(), name='api_login'),
+    url(r'^api/register/$', UserCreateAPIView.as_view(), name='api_register'),
 
     url(r'^accounts/profile/$', profile_view, name='profile'),
+    url(r'^login/$', login, name='login'),
+    url(r'^logout/$', logout, name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
+from django.core.urlresolvers import reverse
 from django.db.models import Q
 from django.contrib.auth import get_user_model
+
+import json
+import requests
+from django.template import  RequestContext
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logut
 
 
 from rest_framework.response import Response
@@ -71,20 +81,49 @@ from .models import *
 from .forms import *
 
 def profile_view(request):
+    if request.user.id:
+        acc = get_object_or_404(
+            Account,
+            id=request.user.pk
+        )
+        form = ProfileForm()
 
-    acc = get_object_or_404(
-        Account,
-        id=request.user.pk
-    )
-    form = ProfileForm()
-
-    if request.method == 'POST':
-        form = ProfileForm(request.POST,request.FILES, instance=acc)
-        if request.user.is_authenticated():
-            if form.is_valid():
-                # acc.profile_picture.delete()
-                #TODO:trzeba by ogarnać jak kasować zdjęcia z bazy
-                form.save()
+        if request.method == 'POST':
+            form = ProfileForm(request.POST,request.FILES, instance=acc)
+            if request.user.is_authenticated():
+                if form.is_valid():
+                    # acc.profile_picture.delete()
+                    #TODO:trzeba by ogarnać jak kasować zdjęcia z bazy
+                    form.save()
 
 
-    return render(request, 'profile.html', {'user': acc,'form':form})
+        return render(request, 'profile.html', {'user': acc,'form':form})
+    else:
+        return HttpResponseRedirect(reverse('accounts:login'))
+
+
+def login(request):
+    error = ''
+
+    if request.method == "POST":
+        #TODO zmienić na dynamiczny link
+        login = requests.post('http://127.0.0.1:8000' + reverse('accounts:api_login'),
+                              data={'username': request.POST['username'], 'password': request.POST['password']})
+        response = json.loads(login.text)
+
+        if response.get('non_field_errors', False):
+            error = response['non_field_errors']
+        elif response.get('token', False) and response.get('username', False):
+            username = response.get('username')
+            password = request.POST['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    auth_login(request, user)
+                    return HttpResponseRedirect("/accounts/profile")
+    return render_to_response("login.html", {"error": error}, RequestContext(request))
+
+
+def logout(request):
+    auth_logut(request)
+    return HttpResponseRedirect('/')
